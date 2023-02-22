5fe13177c85e3729979b90c1ff98ff38d520bada
Lucas Reis <lucasreis_9@hotmail.com>
2023-02-22 12:45:15 -0300

    Trabalhando com Futuro

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:async/async.dart';
+import 'dart:async';
 import 'dart:convert';
 
 const request =
     'https://api.hgbrasil.com/finance?format=json-cors&key=ba136413';
 
 void main() async {
-  http.Response response = await http.get(Uri.parse(request));
-  print(json.decode(response.body)["results"]["currencies"]['USD']);
-
   runApp(MaterialApp(
-    home: Container(),
+    home: Home(),
+    debugShowCheckedModeBanner: false,
   ));
+}
+
+Future<Map> getData() async {
+  http.Response response = await http.get(Uri.parse(request));
+  return json.decode(response.body);
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text(
+          " \$ Conversor \$",
+        ),
+        backgroundColor: Colors.amber,
+        centerTitle: true,
+      ),
+      body:
+    );
+  }
 }
